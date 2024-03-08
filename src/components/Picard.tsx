f55b34f2a114745e1bca9d86c1922f97738678c0
vanshgarg03 <vg9130@srmist.edu.in>
2024-03-08 23:28:03 +0530

    update imagesize

--- src/components/Picard.tsx
+++ src/components/Picard.tsx
@@ -14,14 +14,15 @@ const Picard = ({ src, title, desc }: cardProps) => {
   return (
     <div className="flex flex-col gap-y-3 snap-center px-2">
       <div className="group rounded-md overflow-hidden relative shadow-lg">
-        <Image
-          src={src}
-          alt={`${title} picture`}
-          width={808}
-          height={632}
-          className="group-hover:scale-110 group-hover:brightness-75 brig transition-all duration-300"
-        />
-        <div className="absolute top-0 opacity-0 group-hover:opacity-100 w-full flex justify-between transition-opacity duration-300 p-4">
+      <Image
+  src={src}
+  alt={`${title} picture`}
+  width={808}
+  height={632}
+  className="group-hover:scale-110 group-hover:brightness-75 brig transition-all duration-300"
+  style={{ width: '808px', height: '480px' }}
+/>
+<div className="absolute top-0 opacity-0 group-hover:opacity-100 w-full flex justify-between transition-opacity duration-300 p-4">
           <Link
             href=""
             className="flex items-center gap-x-2 px-3 py-2 bg-gray-700 text-gray-100 rounded-3xl bg-opacity-70"
